d88882364a384d7197d80a9921c7dee890a0f1be
vagrant <vagrant@localhost.localdomain>
2020-10-23 10:02:49 +0000

    add:devise,users,homes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  resources :users
 end
