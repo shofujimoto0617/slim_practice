c53b300c834930b6f822fb8d3c86cab2e10da8dd
vagrant <vagrant@localhost.localdomain>
2020-10-23 12:31:10 +0000

    update:layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resourse)
-	users_path
+	user_path(current_user)
   end
 
   def after_sign_out_path_for(resourse)
